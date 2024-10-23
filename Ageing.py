931c2a30e417bde64df9c1f1bd6c92262d377d55
D.S.S.Gangadhar.Prathap <166209332+GangadharPrathap@users.noreply.github.com>
2024-10-23 15:57:02 +0530

    Create Ageing.py

---
+++ Ageing.py
@@ -0,0 +1,8 @@
+'''
+Chef's current age is 20 years, while Chefina's current age is 10 years.
+Determine Chefina's age when Chef will be X years old.
+Note: Assume that Chef and Chefina were born on same day and same month (just different year).'''
+n=int(input())
+for i in range(n):
+    a=int(input())
+    print(a-10)
