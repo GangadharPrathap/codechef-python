939c935a5bf7ab7e97a1a1f569c4217bcb8942c3
D.S.S.Gangadhar.Prathap <166209332+GangadharPrathap@users.noreply.github.com>
2024-12-08 12:17:17 +0530

    Create Discount.py

---
+++ Discount.py
@@ -0,0 +1,9 @@
+'''Alice buys a toy with a selling price of 
+100
+100 rupees. There is a discount of 
+x
+x percent on the toy. Find the amount Alice needs to pay for it.'''
+n=int(input())
+for i in range(n):
+    a=int(input())
+    print(100-a)
