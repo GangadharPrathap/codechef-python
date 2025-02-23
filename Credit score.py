e84ce0422de71b7e11cf91acf133ab7e54abd139
D.S.S.Gangadhar.Prathap <166209332+GangadharPrathap@users.noreply.github.com>
2025-02-23 15:52:23 +0530

    Create  Credit score.py

---
+++ Credit score.py
@@ -0,0 +1,9 @@
+'''
+To access CRED programs, one needs to have a credit score of 750 or more.
+Given that the present credit score is X, determine if one can access CRED programs or not
+If it is possible to access CRED programs, output YES, otherwise output NO.
+'''n=int(input())
+if n>=750:
+    print('YES')
+else:
+    print('NO')
